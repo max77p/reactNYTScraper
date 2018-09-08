0d6895a2da3ab0d23db8d38219a4006ec997bd38
max77p <shanindrakumar@gmail.com>
2018-09-08 16:00:00 -0400

    update

---
+++ server.js
@@ -0,0 +1,25 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const mongoose=require("mongoose");
+const routes=require("./routes");
+const app=express();
+const PORT=process.env.PORT || 3001;
+
+//define middleware here
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+if(process.env.NODE==="production"){
+    app.use(express.static("client/build"));
+};
+
+//add routes
+app.use(routes);
+
+//connect to mongodb
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactnytscrape");
+
+//start the api server
+app.listen(PORT,function(){
+    console.log(`API Server now listening on PORT ${PORT}`);
+})
