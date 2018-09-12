ec68a304cc331e7a95d53eb537a81739f159948f
max77p <shanindrakumar@gmail.com>
2018-09-12 01:47:41 -0400

    duplicate saves not allowed fixed

--- models/article.js
+++ models/article.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const articleSchema = new Schema({
-  snippet: { type: String, required: true },
+  snippet: { type: String, required: true,index:{unique:true,dropDups:true}},
   link: { type: String, required: true },
   pubdate: { type: String, required: false },
   author: { type: String, required: false }
