657071404023f4855474e6bae4826be0b1f8bb6a
max77p <shanindrakumar@gmail.com>
2018-09-12 19:37:28 -0400

    update

--- client/src/components/Results/Results.js
+++ client/src/components/Results/Results.js
@@ -12,24 +12,35 @@ export const Results = props => {
       var pubdate;
       var author;
       // console.log(x)
-      if ("pub_date" in x) {//check if publication date exists
+      if ("pub_date" in x) {
+        //check if publication date exists
         pubdate = x.pub_date.slice(0, 10);
         // console.log(pubdate);
       } else {
         pubdate = "Publication date not available";
       }
 
-      if ("byline" in x) {//check if author name exists in database
+      if ("byline" in x) {
+        //check if author name exists in database
         author = x["byline"].original;
         // console.log(author);
       } else {
         author = "Author not available";
       }
-      content.push(<List articles={x} key={x._id} pubdate={pubdate} author={author} click={props.click} setting="search"/>);
+      content.push(
+        <List
+          articles={x}
+          key={x._id}
+          pubdate={pubdate}
+          author={author}
+          click={props.click}
+          setting="search"
+        />
+      );
     });
   }
 
-  return (
+  return ([
     <div className="panel panel-info">
       <div className="panel-heading">
         <i className="far fa-newspaper fa-fw ficon" /> {props.heading}
@@ -37,7 +48,11 @@ export const Results = props => {
       <div className="panel-body articleSection" />
       <ol className="ordered list-group">{content}</ol>
     </div>
-  );
+//   <nav aria-label="...">
+//   <ul class="pagination">
+//     <li class="disabled"><a href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>
+//     <li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>
+//   </ul>
+// </nav>
+  ]);
 };
-
-
